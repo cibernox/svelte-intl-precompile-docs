5c4c9e25dea7f6e9c215733712c63d28ee89a9fc
Miguel Camba <miguel.camba@gmail.com>
2021-10-20 01:01:30 +0200

    Very basic structure

---
+++ src/routes/getting-started.svelte
@@ -0,0 +1,9 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Getting started</title>
+</svelte:head>
+
+Getting started
---
+++ src/routes/introduction.svelte
@@ -0,0 +1,9 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Introduction</title>
+</svelte:head>
+
+Introduction
