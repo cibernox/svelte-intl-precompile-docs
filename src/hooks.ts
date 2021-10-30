552233c786457f2a4d5a3ae8ca26803a39ed972a
Miguel Camba <miguel.camba@gmail.com>
2021-10-30 23:19:46 +0200

    Add parens in hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -27,6 +27,6 @@ export const handle: Handle = async ({ request, resolve }) => {
 
 export function getSession(request) {
 	const cookies = cookie.parse(request.headers.cookie || '');
-  let acceptedLanguage = cookies.lang || request.headers["accept-language"] && request.headers["accept-language"].split(',')[0];
+  let acceptedLanguage = cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
   return { acceptedLanguage };
 }
