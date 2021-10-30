5f04407635ca7a9da5184afdc658cf0ead53ee27
Miguel Camba <miguel.camba@gmail.com>
2021-10-31 01:15:08 +0200

    Try this instead

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
 import cookie from 'cookie';
 
 export function getSession(request) {
-	const cookies = cookie.parse(request.headers.cookie || '');
-  let acceptedLanguage = cookies; // 		.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
+	// const cookies = cookie.parse(request.headers.cookie || '');
+  let acceptedLanguage = JSON.stringify(request.headers); // 		cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
   return { acceptedLanguage };
 }
