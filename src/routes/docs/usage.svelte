9fa18639ba073aec4f04c68ee9ea38c36c270462
Miguel Camba <miguel.camba@gmail.com>
2021-10-30 08:56:09 +0200

    Minor readability adjustment

--- src/routes/docs/usage.svelte
+++ src/routes/docs/usage.svelte
@@ -95,7 +95,10 @@
 	import { t, locale, locales } from 'svelte-intl-precompile';
 </script>
 {#each $locales as loc}
-	<button type="button" class={loc === $locale && 'current'} on:click={() => $locale = loc}>{loc}</button>
+	<button 
+		type="button" 
+		class={loc === $locale && 'current'} 
+		on:click={() => $locale = loc}>{loc}</button>
 {/each}
 `}</Codeblock>
 
