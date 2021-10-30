967bd7e3f3ca5465a5a7860a77ede95986ba89ba
Miguel Camba <miguel.camba@gmail.com>
2021-10-31 01:19:25 +0200

    My last idea

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,6 @@ import cookie from 'cookie';
 
 export function getSession(request) {
 	// const cookies = cookie.parse(request.headers.cookie || '');
-  let acceptedLanguage = JSON.stringify(request.headers); // 		cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
+  let acceptedLanguage = JSON.stringify(request); // 		cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
   return { acceptedLanguage };
 }
