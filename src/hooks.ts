2a8765ec7e0f1de336fbc8064a35d3c82b924a6c
Miguel Camba <miguel.camba@gmail.com>
2021-10-31 22:55:54 +0100

    This should work but it doesn't

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,8 @@
-// import cookie from 'cookie';
+import cookie from 'cookie';
 
 export function getSession(request) {
-	// const cookies = cookie.parse(request.headers.cookie || '');
-  let acceptedLanguage = JSON.stringify(request); // 		cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
-  console.log(`[INFO] Request received in getSession is ${acceptedLanguage}`);
+	const cookies = cookie.parse(request.headers.cookie || '');
+  let acceptedLanguage = cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
+  console.log(`[INFO] Request received in getSession is ${JSON.stringify(request)}`);
   return { acceptedLanguage };
 }
