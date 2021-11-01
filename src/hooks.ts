a33f96eb65e11bf2cba6103802de7ca4d75d7c89
Miguel Camba <miguel.camba@gmail.com>
2021-11-01 21:22:00 +0100

    Save slightly different approach

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,22 @@
 import cookie from 'cookie';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ request, resolve }) => {
+	const cookies = cookie.parse(request.headers.cookie || '');
+	const locale = cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
+	request.locals.locale = locale;
+
+	// TODO https://github.com/sveltejs/kit/issues/1046
+	if (request.query.has('_method')) {
+		request.method = request.query.get('_method').toUpperCase();
+	}
+
+	const response = await resolve(request);
+
+	return response;
+};
 
 export function getSession(request) {
-	const cookies = cookie.parse(request.headers.cookie || '');
-  let acceptedLanguage = cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
   console.log(`[INFO] Request received in getSession is ${JSON.stringify(request)}`);
-  return { acceptedLanguage };
+  return { acceptedLanguage: request.locals.locale };
 }
