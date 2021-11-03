59c852b40b5dc52b987ab2f85e4b7954b6541de7
Miguel Camba <miguel.camba@gmail.com>
2021-11-03 16:00:53 +0100

    Save progress

--- src/routes/[...lang]/docs/icu-crash-course.svelte
+++ src/routes/[...lang]/docs/icu-crash-course.svelte
@@ -4,11 +4,18 @@
 
 <script lang="ts">
 	import { t } from 'svelte-intl-precompile';
-	// import Codeblock from '$lib/codeblock.svelte';
-	// import CodeInline from '$lib/code-inline.svelte';
 	import scrollToHash from '$lib/scroll-to-hash';
 
 	scrollToHash();
+	let interpolation = 'orange';
+	let guest = 'John';
+	let plural1 = 0;
+	let plural2 = 0;
+	let guestCount = 0;
+	let interpolationKey = "Your have {numCats, plural, one {one cat} other {# cats}}";
+	let simplePlural = "Your have {numCats, plural, one {one cat} other {# cats}}";
+	let complexPlural = "Your have {numCats, plural, \n  =0 {no cats at all} \n  =1 {one single cat} \n  =2 {a couple cats} \n  =3 {a trio of cats} \n  =12 {a dozen cats} \n  other {exactly # cats}}";
+	let offsetPlural = "Mary {guestCount, plural, offset:1 \n  =0 {does not give a party.} \n  =1 {invites {guest} to her party.} \n  =2 {invites {guest} and one other person to her party.} \n  other {invites {guest} and # other people to her party.}}";
 </script>
 
 <svelte:head>
@@ -61,19 +68,125 @@
 <table class="table-auto w-full text-left">
   <thead>
     <tr>
-      <th>Definition</th>
-      <th>Invocation</th>
-      <th>Result</th>
+      <th>{$t('icu-crash-course.table-heads.definition')}</th>
+      <th>{$t('icu-crash-course.table-heads.definition')}</th>
+      <th>{$t('icu-crash-course.table-heads.output')}</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>{@html $t('icu-crash-course.rows.interpolations-1')}</td>
-      <td>{@html $t('icu-crash-course.rows.interpolations-2')}</td>
-      <td>{@html $t('icu-crash-course.rows.interpolations-3')}</td>
+      <td>
+				<pre><code>{interpolationKey}</code></pre>
+			</td>
+      <td>
+				<input 
+					class="shadow border rounded py-2 px-3 text-gray-700 focus:outline-none" 
+					type="text" 
+					bind:value={interpolation} 
+					placeholder="chosen">
+			</td>
+      <td>{$t('icu-crash-course.table-cells.interpolations-2', { values: { chosen: interpolation } })}</td>
     </tr>
 	</tbody>
 </table>
+
+<h2 class="text-2xl font-semibold">{$t('icu-crash-course.subsection.plurals')}</h2>
+
+<p>
+	The second most used feature in any app is pluralization. The ICU syntax has a dedicated <code class="inline">plural</code>
+	helper to define plural translations that from very simple to quite complicated, all within the translation itself.
+</p>
+
+<div>
+	Each path for the plural is prefixed with the numeric qualifier. The possible qualifiers are:
+	<ul class="list-disc list-inside ml-8">
+		<li>zero</li>
+		<li>one (singular)</li>
+		<li>two (dual)</li>
+		<li>few (paucal)</li>
+		<li>many (also used for fractions)</li>
+		<li>other (general plural form. The one used on languages with only one plural)</li>
+	</ul>
+	Let's se some examples first:
+</div>
+
+<table class="table-auto w-full text-left">
+  <thead>
+    <tr>
+      <th>{$t('icu-crash-course.table-heads.definition')}</th>
+      <th>{$t('icu-crash-course.table-heads.values')}</th>
+      <th>{$t('icu-crash-course.table-heads.output')}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+				<pre><code>{simplePlural}</code></pre>
+			</td>
+      <td>
+				<input 
+					class="shadow border rounded py-2 px-3 text-gray-700 focus:outline-none" 
+					type="number" 
+					bind:value={plural1} 
+					min="0">
+			</td>
+      <td>{$t('icu-crash-course.table-cells.plurals-simple', { values: { numCats: plural1 } })}</td>
+    </tr>
+    <tr>
+      <td>
+				<pre><code>{complexPlural}</code></pre>
+			</td>
+      <td>
+				<input 
+					class="shadow border rounded py-2 px-3 text-gray-700 focus:outline-none" 
+					type="number" 
+					bind:value={plural2} 
+					min="0">
+			</td>
+      <td>{$t('icu-crash-course.table-cells.plurals-complex', { values: { count: plural2 } })}</td>
+    </tr>
+    <tr>
+      <td>
+				<pre><code>{offsetPlural}</code></pre>
+			</td>
+      <td>
+				<input 
+					class="shadow border rounded py-2 px-3 text-gray-700 focus:outline-none" 
+					type="number" 
+					bind:value={guestCount} 
+					min="0">
+				<input 
+					class="shadow border rounded py-2 px-3 text-gray-700 focus:outline-none" 
+					type="text" 
+					bind:value={guest} 
+					placeholder="guest"
+					min="0">
+			</td>
+      <td>{$t('icu-crash-course.table-cells.plurals-offset', { values: { guest, guestCount } })}</td>
+    </tr>
+	</tbody>
+</table>
+
+<p>
+	Some languages like English only leverage <code class="inline">one</code> and <code class="inline">other</code> but others
+	will be able to use the best plural form. In particular threshold value that divides <code class="inline">few</code> and <code class="inline">many</code>
+	is heavily cultural.
+</p>
+
+<p>
+	You can also specify translations for exact values using <code class="inline">=N</code>. When a number is specified
+	that way that translation will supersede the language's default behavior. <br>
+	For instance, in english you could use <code class="inline">=2</code> or <code class="inline">=12</code> to have different
+	translations specifically for a couple and a dozen instead of using the generic plural.
+</p>
+
+<p>
+	Lastly, plurals can also make us of the hashtag to print as number the value being used in the plural, and
+	optionally the helper can receive an offset that will be substracted to the value in the hashtag.
+</p>
+
+<h2 class="text-2xl font-semibold">{$t('getting-started.subsection.installation')}</h2>
+
 
 <!-- 
 <h2 class="text-2xl font-semibold">{$t('getting-started.subsection.installation')}</h2>
