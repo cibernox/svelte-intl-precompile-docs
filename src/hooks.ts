5354770a08186758e095135f6910986ea003ca5f
Miguel Camba <miguel.camba@gmail.com>
2022-01-28 18:54:37 +0100

    Update to latest sveltekit

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,23 +1,18 @@
 import cookie from 'cookie';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ request, resolve }) => {
-	const cookies = cookie.parse(request.headers.cookie || '');
-	const locale = cookies.lang || (request.headers["accept-language"] && request.headers["accept-language"].split(',')[0]);
+export const handle: Handle = async ({ event, resolve }) => {
+	const cookies = cookie.parse(event.request.headers.get('cookie') ?? '');
+	const locale = cookies.lang || (event.request.headers.get("accept-language") && event.request.headers.get("accept-language").split(',')[0]);
 	// console.log('Locale extracted from the cookies is:', locale);
 	// console.log('request is:', JSON.stringify(request));
-	request.locals.locale = locale;
+	event.locals.locale = locale;
 
-	// TODO https://github.com/sveltejs/kit/issues/1046
-	if (request.url?.searchParams.has('_method')) {
-		request.method = request.url?.searchParams.get('_method').toUpperCase();
-	}
-
-	const response = await resolve(request);
+	const response = await resolve(event);
 
 	return response;
 };
 
-export function getSession(request) {
-  return { acceptedLanguage: request.locals.locale };
+export function getSession(event) {
+  return { acceptedLanguage: event.locals.locale };
 }
