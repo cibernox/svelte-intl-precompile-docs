fb9faa20bef250adbbea115b1d1941b3f4710960
Miguel Camba <miguel.camba@gmail.com>
2022-09-19 18:02:08 +0200

    Try to simplify hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,13 @@
 import cookie from 'cookie';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle: Handle = ({ event, resolve }) => {
 	const cookies = cookie.parse(event.request.headers.get('cookie') ?? '');
 	const locale = cookies.lang || (event.request.headers.get("accept-language") && event.request.headers.get("accept-language").split(',')[0]);
 	// console.log('Locale extracted from the cookies is:', locale);
 	// console.log('request is:', JSON.stringify(request));
 	event.locals.locale = locale;
-
-	const response = await resolve(event);
-
-	return response;
+	return resolve(event);
 };
 
 export function getSession(event) {
