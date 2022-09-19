ec2a541eb51e305a830eb058632a5f163446c9d2
Miguel Camba <miguel.camba@gmail.com>
2022-09-19 17:50:37 +0200

    Switch to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,8 +12,20 @@ const config = {
   ],
 
   kit: {
-    adapter: netlify(),
-    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    }),
   },
 };
 
